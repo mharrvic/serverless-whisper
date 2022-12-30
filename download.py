c42b69c8cc0ca2e8e125d4af588021a448e51353
mac <mharrvic@gmail.com>
2022-12-30 21:48:08 +0800

    Init

---
+++ download.py
@@ -0,0 +1,15 @@
+# In this file, we define download_model
+# It runs during container build time to get model weights built into the container
+
+# In this example: A Huggingface BERT model
+
+import torch
+import whisper
+
+
+def download_model():
+    model = whisper.load_model("base")
+
+
+if __name__ == "__main__":
+    download_model()
